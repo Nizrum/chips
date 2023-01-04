d7e9c1a49fb6ce60f195ac69c2777309bf2f2152
Nizrum <62435919+Nizrum@users.noreply.github.com>
2023-01-04 16:29:03 +0400

    Реализовал работу корзины
    Реализовал работу корзины.

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,40 +1,42 @@
-const darkModeBtn = document.querySelector(".header__dark-mode-button");
-const darkModeIcon = darkModeBtn.querySelector("img");
-const body = document.querySelector("body");
-const search = document.querySelector(".search");
-const cartBtn = document.querySelector(".header__cart-button");
-const mainTitle = document.querySelector(".main__title");
-const productTexts = document.querySelectorAll(".product__text");
-const amountCounter = document.querySelector(".amount-counter");
-const addButton = document.querySelector(".add-to-cart-button");
-const cart = document.querySelector(".cart");
+(function () {
+    const darkModeBtn = document.querySelector(".header__dark-mode-button");
+    const darkModeIcon = darkModeBtn.querySelector("img");
+    const body = document.querySelector("body");
+    const search = document.querySelector(".search");
+    const cartBtn = document.querySelector(".header__cart-button");
+    const mainTitle = document.querySelector(".main__title");
+    const productTexts = document.querySelectorAll(".product__text");
+    const amountCounter = document.querySelector(".amount-counter");
+    const addButton = document.querySelector(".add-to-cart-button");
+    const cart = document.querySelector(".cart");
 
-function toggleMode() {
-    darkModeIcon.src = `images/${darkModeIcon.src.includes("dark") ? "light" : "dark"}-mode.svg`;
-    body.classList.toggle("dark-mode");
-    mainTitle.classList.toggle("main__title_dark-mode");
-    search.classList.toggle("search_dark-mode");
-    cartBtn.classList.toggle("header__cart-button_dark-mode");
-    amountCounter.classList.toggle("amount-counter_dark-mode");
-    addButton.classList.toggle("add-to-cart-button_dark-mode");
-    cart.classList.toggle("cart_dark-mode");
-    document.querySelectorAll(".ingredient__text").forEach((item) => {
-        item.classList.toggle("ingredient__text_dark-mode");
+    function toggleMode() {
+        darkModeIcon.src = `images/${darkModeIcon.src.includes("dark") ? "light" : "dark"}-mode.svg`;
+        body.classList.toggle("dark-mode");
+        mainTitle.classList.toggle("main__title_dark-mode");
+        search.classList.toggle("search_dark-mode");
+        cartBtn.classList.toggle("header__cart-button_dark-mode");
+        amountCounter.classList.toggle("amount-counter_dark-mode");
+        addButton.classList.toggle("add-to-cart-button_dark-mode");
+        cart.classList.toggle("cart_dark-mode");
+        document.querySelectorAll(".ingredient__text").forEach((item) => {
+            item.classList.toggle("ingredient__text_dark-mode");
+        });
+        productTexts.forEach((item) => {
+            item.classList.toggle("product__text_dark-mode");
+        });
+    }
+
+    darkModeBtn.addEventListener("click", function () {
+        toggleMode();
+        localStorage.setItem("darkmode", localStorage.getItem("darkmode") == "dark" ? "light" : "dark");
     });
-    productTexts.forEach((item) => {
-        item.classList.toggle("product__text_dark-mode");
-    });
-}
 
-darkModeBtn.addEventListener("click", function () {
-    toggleMode();
-    localStorage.setItem("darkmode", localStorage.getItem("darkmode") == "dark" ? "light" : "dark");
-});
+    if (!localStorage.getItem("darkmode")) {
+        localStorage.setItem("darkmode", "light");
+    }
 
-if (!localStorage.getItem("darkmode")) {
-    localStorage.setItem("darkmode", "light");
-}
-
-if (localStorage.getItem("darkmode") == "dark") {
-    toggleMode();
-}
+    if (localStorage.getItem("darkmode") == "dark") {
+        toggleMode();
+    }
+})();
--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,20 +1,21 @@
-const title = document.querySelector(".main__title");
-const subtitle = document.querySelector(".main__subtitle");
-const addBtn = document.querySelector(".add-to-cart-button");
-const image = document.querySelector(".main__image");
-const ingredients = document.querySelector(".main__ingredients");
-const productsList = document.querySelector(".other__list");
-let productsArr;
+(function () {
+    const title = document.querySelector(".main__title");
+    const subtitle = document.querySelector(".main__subtitle");
+    const addBtn = document.querySelector(".add-to-cart-button");
+    const image = document.querySelector(".main__image");
+    const ingredients = document.querySelector(".main__ingredients");
+    const productsList = document.querySelector(".other__list");
 
-function renderProduct(id) {
-    let currentProduct = productsArr.filter((p) => p["id"] == id)[0];
-    title.textContent = currentProduct["name"];
-    subtitle.textContent = currentProduct["description"];
-    addBtn.dataset.id = currentProduct["id"];
-    image.src = currentProduct["img"];
-    productsList.innerHTML = "";
-    for (let product of productsArr.filter((p) => p["id"] != id)) {
-        productsList.innerHTML += `
+    const renderProduct = (id) => {
+        let productsArr = JSON.parse(localStorage.getItem("allProducts"));
+        let currentProduct = productsArr.filter((p) => p["id"] == id)[0];
+        title.textContent = currentProduct["name"];
+        subtitle.textContent = currentProduct["description"];
+        addBtn.dataset.id = currentProduct["id"];
+        image.src = currentProduct["img"];
+        productsList.innerHTML = "";
+        for (let product of productsArr.filter((p) => p["id"] != id)) {
+            productsList.innerHTML += `
         <div class="other__product product">
             <img src="${product["img"]}" alt="a pack of chips" class="product__image">
             <div class="product__text">
@@ -24,31 +25,34 @@ function renderProduct(id) {
             </div>
         </div>
         `;
-    }
-    ingredients.innerHTML = "";
-    for (let ingredient of currentProduct["ingredients"]) {
-        ingredients.innerHTML += `
+        }
+        ingredients.innerHTML = "";
+        for (let ingredient of currentProduct["ingredients"]) {
+            ingredients.innerHTML += `
         <div class="main__ingredient ingredient">
-            <span class="ingredient__text ${localStorage.getItem('darkmode') == 'dark' ? 'ingredient__text_dark-mode' : ''}">${ingredient}</span>
+            <span class="ingredient__text ${
+                localStorage.getItem("darkmode") == "dark" ? "ingredient__text_dark-mode" : ""
+            }">${ingredient}</span>
             <div class="ingredient__image ingredient__image_${ingredient}">
                 <img src="images/${ingredient}.png" alt="ingredient image" class="ingredient__img">
             </div>
         </div>
         `;
-    }
-}
+        }
+    };
 
-fetch("https://chips-73f39-default-rtdb.firebaseio.com/db.json")
-    .then((response) => {
-        return response.text();
-    })
-    .then((response) => {
-        productsArr = JSON.parse(response);
-        renderProduct("001");
+    fetch("https://chips-73f39-default-rtdb.firebaseio.com/db.json")
+        .then((response) => {
+            return response.text();
+        })
+        .then((response) => {
+            localStorage.setItem("allProducts", response);
+            renderProduct("001");
+        });
+
+    productsList.addEventListener("click", (event) => {
+        if (event.target.classList.contains("product__button")) {
+            renderProduct(event.target.dataset.id);
+        }
     });
-
-productsList.addEventListener("click", (event) => {
-    if (event.target.classList.contains("product__button")) {
-        renderProduct(event.target.dataset.id);
-    }
-});
+})();
